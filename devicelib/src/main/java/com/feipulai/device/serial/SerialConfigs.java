64ba099fae0dea45e06c816e9a5a8258fc409651
pengjf <1042293434@qq.com>
2019-06-20 16:08:20 +0800

    修改摸高无线模式

--- devicelib/src/main/java/com/feipulai/device/serial/SerialConfigs.java
+++ devicelib/src/main/java/com/feipulai/device/serial/SerialConfigs.java
@@ -75,11 +75,19 @@ public class SerialConfigs {
 
     //离地高度设置范围为0-255
     public static byte[] CMD_SARGENT_JUMP_GET_SET_0(int offGroundDistance) {
-        byte[] data = {0X54, 0X44, 00, 0X10, 00, 0x01, 00, 0x06, 00, 00, 00, 00, 00, 0x00, 0x27, 0x0d};
+        byte[] data = {0X54, 0X44, 00, 0X10, 01, 0x01, 00, 0x06, 00, 00, 00, 00, 00, 0x00, 0x27, 0x0d};
         data[8] = (byte) ((offGroundDistance >> 8) & 0xff);// 次低位
         data[9] = (byte) (offGroundDistance & 0xff);// 最低位
+
+        int sum = 0;
+        for (int i = 2; i <= 12; i++) {
+            sum += data[i] & 0xff;
+        }
+        data[13] = (byte)sum;
         return data;
     }
+
+
 
     //MEDICINE BALL
     public static final byte[] CMD_MEDICINE_BALL_EMPTY = {0X54, 0X44, 00, 0x0c, 00, 0x07, 00, 00, 00, 0, 0x27, 0x0d};
