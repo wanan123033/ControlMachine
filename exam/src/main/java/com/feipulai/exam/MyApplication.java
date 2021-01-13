8a22978cf75ceb46a7053617d500369d663e8ee0
921112138@qq.com <402066zzs>
2021-01-13 10:52:28 +0800

    激活

--- exam/src/main/java/com/feipulai/exam/MyApplication.java
+++ exam/src/main/java/com/feipulai/exam/MyApplication.java
@@ -52,13 +52,13 @@ public class MyApplication extends MultiDexApplication {
         super.onCreate();
         instance = this;
 //        LogUtils.initLogger(true, true, PATH_LOG_NAME);
-//        CrashHandler.getInstance().init(this);
-//        CrashHandler.getInstance().setUploadOpersion(new CrashHandler.UploadOpersion() {
-//            @Override
-//            public void upload(String erroMsg) {
-//                new HttpSubscriber().uploadLog(erroMsg);
-//            }
-//        });
+        CrashHandler.getInstance().init(this);
+        CrashHandler.getInstance().setUploadOpersion(new CrashHandler.UploadOpersion() {
+            @Override
+            public void upload(String erroMsg) {
+                new HttpSubscriber().uploadLog(erroMsg);
+            }
+        });
         SettingHelper.init(this);
         BlueToothHelper.init(this);
         TOKEN = SharedPrefsUtil.getValue(this, SharedPrefsConfigs.DEFAULT_PREFS, SharedPrefsConfigs.TOKEN, "");
--- exam/src/main/java/com/feipulai/exam/config/SharedPrefsConfigs.java
+++ exam/src/main/java/com/feipulai/exam/config/SharedPrefsConfigs.java
@@ -109,5 +109,5 @@ public class SharedPrefsConfigs {
     public static final String MACHINE_PORT = "machine_port";//连接设备端口
     public static final String MACHINE_SERVER_PORT = "machine_server_port";//本机作为服务器的端口
     public static final String CAMERA_IP = "camera_ip";//摄像头ip
-    public static final String LAST_DOWNLOAD_TIME = "LAST_DOWNLOAD_TIME_EXAM";
+//    public static final String LAST_DOWNLOAD_TIME = "LAST_DOWNLOAD_TIME_EXAM";
 }
