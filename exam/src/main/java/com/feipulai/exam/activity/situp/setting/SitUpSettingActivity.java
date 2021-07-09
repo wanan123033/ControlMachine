96f12ccd2683d51152ccfe686d2b22e83eab333f
彭江锋 <1042293434@qq.com>
2021-07-09 14:33:57 +0800

    仰卧起坐通信质量测试

--- exam/src/main/java/com/feipulai/exam/activity/situp/setting/SitUpSettingActivity.java
+++ exam/src/main/java/com/feipulai/exam/activity/situp/setting/SitUpSettingActivity.java
@@ -1,13 +1,18 @@
 package com.feipulai.exam.activity.situp.setting;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
 
+import com.feipulai.exam.R;
 import com.feipulai.exam.activity.jump_rope.base.setting.AbstractRadioSettingActivity;
 import com.feipulai.exam.activity.jump_rope.base.setting.AbstractRadioSettingPresenter;
+import com.feipulai.exam.activity.setting.CorrespondTestActivity;
 
-public class SitUpSettingActivity extends AbstractRadioSettingActivity {
+public class SitUpSettingActivity extends AbstractRadioSettingActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,6 +20,10 @@ public class SitUpSettingActivity extends AbstractRadioSettingActivity {
         llTestMin.setVisibility(View.VISIBLE);
         llTestMax.setVisibility(View.VISIBLE);
         llTestLed.setVisibility(View.VISIBLE);
+        Button btnConnect = findViewById(R.id.btn_connect);
+        btnConnect.setVisibility(View.VISIBLE);
+        btnConnect.setOnClickListener(this);
+
     }
 
     @Override
@@ -22,4 +31,12 @@ public class SitUpSettingActivity extends AbstractRadioSettingActivity {
         return new SitUpSettingPresenter(this, this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_connect:
+                startActivity(new Intent(this, CorrespondTestActivity.class));
+                break;
+        }
+    }
 }
