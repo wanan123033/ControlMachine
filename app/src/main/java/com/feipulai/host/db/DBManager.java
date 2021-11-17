41b177c939abf96528185dcd26de986e181b0e86
921112138@qq.com <402066zzs>
2021-11-17 13:20:06 +0800

    体测0机器码处理

--- app/src/main/java/com/feipulai/host/db/DBManager.java
+++ app/src/main/java/com/feipulai/host/db/DBManager.java
@@ -95,6 +95,10 @@ public class DBManager {
             if (items != null && items.size() != 0) {
                 continue;
             }
+            items = itemDao.queryBuilder().where(ItemDao.Properties.ItemName.eq(TestConfigs.machineNameMap.get(machineCode))).list();
+            if (items != null && items.size() != 0) {
+                continue;
+            }
             switch (machineCode) {
                 case ItemDefault.CODE_HW:
                     insertItem(machineCode, TestConfigs.HEIGHT_ITEM_CODE, "身高", "厘米");
